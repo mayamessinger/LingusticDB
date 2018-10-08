46fcbf6b4d3ff6dde0402970e22363a76e1a3b18
Maya Messinger <maya.messinger@duke.edu>
2018-10-08 16:21:50 -0400

    committed from sub-folder, didn't remove copies of files in main folder

--- load.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Authors VALUES
-('Jane Austen'),
-('Lewis Carrol');
-
-INSERT INTO Books VALUES
-(1, 'Alice in Wonderland', 'Lewis Carrol', 14, 3500),
-(2, 'Pride and Prejudice', 'Jane Austen', 16, 3000),
-(3, 'Sense and Sensibility', 'Jane Austen', 15, 10000);

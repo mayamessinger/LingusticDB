c00b87e02c5b53564908b7f3e7bf49df0a1cc37a
Maya Messinger <maya.messinger@duke.edu>
2018-10-08 16:19:44 -0400

    Updating sample queries and fixing sample db

---
+++ milestone1/README.md
@@ -0,0 +1,6 @@
+# Milestone 1
+
+## Populating sample database
+create.sql and load.sql files contain the tables and views of the sample database, and sample data to populate with
+* dropdb ling; createdb ling; psql ling -af create.sql
+* psql ling -af load.sql
---
+++ milestone1/load.sql
@@ -0,0 +1,8 @@
+INSERT INTO Authors VALUES
+('Austen, Jane'),
+('Carroll, Lewis');
+
+INSERT INTO Books VALUES
+(1, 'Alice in Wonderland', 'Carroll, Lewis', 14, 3500),
+(2, 'Pride and Prejudice', 'Austen, Jane', 16, 3000),
+(3, 'Sense and Sensibility', 'Austen, Jane', 15, 10000);

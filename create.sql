e6824789e01f3f04bbcfe74ec280dc5768495d97
Maya Messinger <maya.messinger@duke.edu>
2018-10-08 12:09:42 -0400

    Some initial database schema, start progress report

---
+++ create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Books
+(uid INTEGER NOT NULL PRIMARY KEY,
+ title VARCHAR(256) NOT NULL,
+ author VARCHAR(256) NOT NULL REFERENCES Authors(name),
+ words_per_sentence INTEGER NOT NULL,
+ num_words INTEGER NOT NULL);
+
+CREATE TABLE Authors
+(name VARCHAR(256) NOT NULL);
